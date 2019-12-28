ab6e6eee0d6625913595a068c50da458796b5a25
Sibian De Gussem <sibian.degussem@ugent.be>
2019-12-28 09:59:01 +0100

    Feedback gekregen en aangepast

--- VoorbeeldExamen/Vraag1/src/ui/TuincentrumApplicatie.java
+++ VoorbeeldExamen/Vraag1/src/ui/TuincentrumApplicatie.java
@@ -1,5 +1,6 @@
 package ui;
 
+
 import domein.Plant;
 import domein.TuincentrumBewerking;
 
@@ -7,9 +8,10 @@ public class TuincentrumApplicatie {
 
     public static void main(String[] args) {
 
-        String[] namen = {"sneeuwklokje","bromelia","gladiool","kerstcactus","heide"};
-        boolean[] buitenplanten = {true,false,true,false,true};
-        double[][] waterPerWeekPerSeizoenen = {{10.5,0.0,0.0,2.5},{2.5,7.5,1.5,0.5},{0.0,15.0,1.5,0.0},{0.5,0.5,0.5,2.5},{0.5,0.5,0.5,0.5}};
+        String[] namen = {"sneeuwklokje", "bromelia", "gladiool", "kerstcactus", "heide"};
+        final String[] seizoenen = {"lente", "zomer", "herfst", "winter"};
+        boolean[] buitenplanten = {true, false, true, false, true};
+        double[][] waterPerWeekPerSeizoenen = {{10.5, 0.0, 0.0, 2.5}, {2.5, 7.5, 1.5, 0.5}, {0.0, 15.0, 1.5, 0.0}, {0.5, 0.5, 0.5, 2.5}, {0.5, 0.5, 0.5, 0.5}};
 
         TuincentrumApplicatie tca = new TuincentrumApplicatie();
         Plant[] plantenArray = tca.vulArray(namen, buitenplanten, waterPerWeekPerSeizoenen);
@@ -17,32 +19,20 @@ public class TuincentrumApplicatie {
         System.out.println(TuincentrumBewerking.maakOverzichtPlanten(plantenArray));
         System.out.printf("Aantal kamerplanten in het tuincentrum: %d%n%n", TuincentrumBewerking.bepaalAantalKamerPlanten(plantenArray));
 
-        /*String waterverbruikPerSeizoen = String.format("Totaal waterverbruik per seizoen:%n" +
-                "lente: %.1f%n" +
-                "zomer: %.1f%n" +
-                "herfst: %.1f%n" +
-                "winter: %.1f%n", // for-lus?
-                );
-
-         */
-
-        System.out.printf("Totaal waterverbruik per seizoen:%n" +
-                "lente: %.1f%n" +
-                "zomer: %.1f%n" +
-                "herfst: %.1f%n" +
-                "winter: %.1f%n",
-                TuincentrumBewerking.berekenTotaalWaterPerWeekPerSeizoen(plantenArray, 0),
-                TuincentrumBewerking.berekenTotaalWaterPerWeekPerSeizoen(plantenArray, 1),
-                TuincentrumBewerking.berekenTotaalWaterPerWeekPerSeizoen(plantenArray, 2),
-                TuincentrumBewerking.berekenTotaalWaterPerWeekPerSeizoen(plantenArray, 3));
+        System.out.println("Totaal waterverbruik per seizoen:");
+        int i = 0;
+        for (String seizoen : seizoenen) {
+            System.out.printf("%s: %.1f%n", seizoen, TuincentrumBewerking.berekenTotaalWaterPerWeekPerSeizoen(plantenArray, i++));
+        }
     }
 
-    private Plant[] vulArray(String[] namen, boolean[] buitenplanten, double[][] waterPerWeekPerSeizoenen){
+    private Plant[] vulArray (String[]namen,boolean[] buitenplanten, double[][] waterPerWeekPerSeizoenen){
         Plant[] plantenArray = new Plant[namen.length];
-        for (int i = 0; i < plantenArray.length ; i++) {
+        for (int i = 0; i < plantenArray.length; i++) {
             plantenArray[i] = new Plant(namen[i], buitenplanten[i], waterPerWeekPerSeizoenen[i]);
         }
         return plantenArray;
     }
 
 }
+
