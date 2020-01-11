5ad6b3e16b62a8ed0f74e673b9904ba31acf4b6d
Sibian De Gussem <sibian.degussem@ugent.be>
2020-01-11 09:49:20 +0100

    Fout H2 oef 4

--- Hoofdstuk2/src/Oefening4.java
+++ Hoofdstuk2/src/Oefening4.java
@@ -4,9 +4,9 @@ public class Oefening4 {
 
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-		System.out.println("Geef een strikt positief geheel getal bestaande uit 5 cijfers, in: ");
+		System.out.print("Geef een strikt positief geheel getal bestaande uit 5 cijfers, in: ");
 		int getal = input.nextInt();
-		System.out.printf("  %d", getal);
+		System.out.printf("%5d%5d%5d%5d%5d", (getal/10000)%10, (getal/1000)%10, (getal/100)%10, (getal/10)%10, (getal%10)%10 );
 	}
 
 }
